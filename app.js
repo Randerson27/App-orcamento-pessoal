5fdc1f3df1f5356e3172cd5df8e805ea80c9837d
Randerson27 <69997038+Randerson27@users.noreply.github.com>
2021-05-19 18:16:47 -0300

    Acessando o LocalStorage

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-class Despesas{
-    constructor(ano, mes, dia, tipo, descricao, valor){
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-    }
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
 }
 
-function cadastrarDespesa(){
+class Bd {
 
-    let ano = document.getElementById('ano').value
-    let mes = document.getElementById('mes').value
-    let dia = document.getElementById('dia').value
-    let tipo = document.getElementById('tipo').value
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
+	constructor() {
+		let id = localStorage.getItem('id')
 
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
 
-    let despesas = new Despesas(
-        ano,
-        mes,
-        dia,
-        tipo,
-        descricao,
-        valor.value
-    )
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
 
-    console.log(despesas)
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
 }
 
+let bd = new Bd()
+
+
+function cadastrarDespesa() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+
+	bd.gravar(despesa)
+}
