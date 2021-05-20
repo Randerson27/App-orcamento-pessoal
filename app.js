6807bd142c13f0d08bf92c83a6227f5b60f76b0c
Randerson27 <69997038+Randerson27@users.noreply.github.com>
2021-05-20 18:00:11 -0300

    Validação de dados de maneira programatica

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ class Despesa {
 		this.valor = valor
 	}
 
-    validarDados(){
-        for(let i in this) {
-            if(this[i] == undefined || this[i] == '' || this[i] == null) {
-                return false
-            }
-        }
-        return true 
-    }
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
 class Bd {
@@ -62,12 +62,28 @@ function cadastrarDespesa() {
 		descricao.value,
 		valor.value
 	)
-    
-    if(despesa.validarDados()) {
-        bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
-    }else{
-        $('#erroGravacao').modal('show')
-    }
-	
+
+
+	if(despesa.validarDados()) {
+		//bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog de sucesso
+		$('#modalRegistraDespesa').modal('show') 
+	} else {
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistraDespesa').modal('show') 
+	}
 }
