dc7104d10faefe918d4c2b465c8ccd8eac21631a
Randerson27 <69997038+Randerson27@users.noreply.github.com>
2021-05-20 18:35:59 -0300

    atualização

--- app.js
+++ app.js
@@ -40,6 +40,31 @@ class Bd {
 
 		localStorage.setItem('id', id)
 	}
+
+	recuperarTodosRegistros() {
+
+		//array de despesas
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+
+		//recuperar todas as despesas cadastradas em localStorage
+		for(let i = 1; i <= id; i++) {
+
+			//recuperar a despesa
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			//existe a possibilidade de haver índices que foram pulados/removidos
+			//nestes casos nós vamos pular esses índices
+			if(despesa === null) {
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
+	}
 }
 
 let bd = new Bd()
@@ -86,4 +111,13 @@ function cadastrarDespesa() {
 		//dialog de erro
 		$('#modalRegistraDespesa').modal('show') 
 	}
+}
+
+function carregaListaDespesas() {
+
+	let despesas = Array()
+
+	despesas = bd.recuperarTodosRegistros() 
+
+	console.log(despesas)
 }
